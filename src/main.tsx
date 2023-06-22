0707120ae4e3c85a1265c8a88f3543e27324e427
Ismael Junior <ismaeljcjunior@gmail.com>
2023-06-22 20:40:40 -0300

    base

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { Login } from './shared/pages/login/Login.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
