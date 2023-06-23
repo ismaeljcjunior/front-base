3d843ad20e6cd390b970c7d72accd18ae5c4c921
Ismael Junior <ismaeljcjunior@gmail.com>
2023-06-23 14:14:09 -0300

    feat page login

---
+++ mock/data.ts
@@ -0,0 +1,7 @@
+
+
+export const users = [
+    { username: 'admin', password: '12345' },
+    { username: 'ismael.junior', password: '123456' },
+    { username: 'user2', password: 'password2' }
+];
--- src/shared/pages/login/Login.tsx
+++ src/shared/pages/login/Login.tsx
@@ -1,10 +1,45 @@
-
+import { useState } from "react";
+import { Row, Col, Input, Button } from 'antd';
 
 export const Login = () => {
-
+    const [dataForm, setDataForm] = useState({
+        username: '',
+        password: ''
+    });
+    const handleLogin = () => {
+        // Lógica para realizar o login
+    };
+    const handleInputChange = (event: { target: { name: string; value: string; }; }) => {
+        const { name, value } = event.target;
+        setDataForm({ ...dataForm, [name]: value });
+    };
     return (
         <>
-            Page Login
+            <Row justify="center" align="middle" style={{ height: '100vh' }}>
+                <Col span={6}>
+                    <div style={{ textAlign: 'center' }}>
+                        <h1>Login</h1>
+                        <Input
+                            placeholder="Username"
+                            name="username"
+                            value={dataForm.username}
+                            onChange={handleInputChange}
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Input.Password
+                            placeholder="Password"
+                            name="password"
+                            value={dataForm.password}
+                            onChange={handleInputChange}
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Button type="primary" onClick={handleLogin}>
+                            Login
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
+            );
         </>
     )
 }
